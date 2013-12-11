976d3534b53b22daf9d49e87c298927488c6e900
Laurent Lasudry <lasudry@gmail.com>
2013-12-11 15:21:44 +0100

    Change application ID from be.gitesdewallonie.allochambresdhotes to be.gitesdewallonie.allochambredhotes

--- src/be/gitesdewallonie/allochambresdhotes/classes/CurrentLocation.java
+++
@@ -1,8 +0,0 @@
-package be.gitesdewallonie.allochambresdhotes.classes;
-
-import java.io.Serializable;
-
-public class CurrentLocation implements Serializable {
-	public double[] coordinates;
-	public String title;
-}
